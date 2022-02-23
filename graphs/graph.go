b96793948151ac695cf62f5ffce6479b79485947
fukaraca <fukaraca@hotmail.com>
2022-02-24 00:45:44 +0300

    modified:   graphs/graph.go 	new file:   graphs/graph_test.go

--- graphs/graph.go
+++ graphs/graph.go
@@ -2,6 +2,7 @@ package graphs
 
 import (
 	"fmt"
+	"sync"
 )
 
 //DirectedGraph is a struct for directed graph
@@ -9,6 +10,7 @@ type directedGraph struct {
 	Vertice map[int]*VertexD
 	arcsLen int
 	vertLen int
+	lock    *sync.Mutex
 }
 
 //VertexD represents vertex for directed graph
@@ -30,6 +32,7 @@ func NewDirected() *directedGraph {
 		Vertice: temp,
 		arcsLen: 0,
 		vertLen: 0,
+		lock:    &sync.Mutex{},
 	}
 }
 
@@ -38,6 +41,7 @@ func (d *directedGraph) AddVertex(id int) error {
 	if d.IsExist(id) {
 		return fmt.Errorf("vertex for given id %d is already exist", id)
 	}
+	d.lock.Lock()
 	tempArc := make(map[*Arc]bool)
 	d.Vertice[id] = &VertexD{
 		Id:     id,
@@ -45,6 +49,7 @@ func (d *directedGraph) AddVertex(id int) error {
 		Arcs:   tempArc,
 	}
 	d.vertLen++
+	d.lock.Unlock()
 	return nil
 }
 
@@ -53,9 +58,11 @@ func (d *directedGraph) AddArc(from, to int) error {
 	if !d.IsExist(from) || !d.IsExist(to) {
 		return fmt.Errorf("at least one of the vertice(%d or/and %d ) is not exist", from, to)
 	}
+
 	if ok, _ := d.IsAdjacent(from, to); ok {
 		return fmt.Errorf("there is already arc from %d to %d", from, to)
 	}
+	d.lock.Lock()
 	neoArc := &Arc{}
 	neoArc.From = d.Vertice[from]
 	neoArc.To = d.Vertice[to]
@@ -63,6 +70,7 @@ func (d *directedGraph) AddArc(from, to int) error {
 	d.Vertice[to].Arcs[neoArc] = false
 	d.Vertice[from].Degree++
 	d.arcsLen++
+	d.lock.Unlock()
 	return nil
 }
 
@@ -96,7 +104,7 @@ func (d *directedGraph) RemoveArc(from, to int) error {
 	}
 	control := false
 	temp := &Arc{}
-	for k, _ := range d.Vertice[from].Arcs {
+	for k := range d.Vertice[from].Arcs {
 		if k.From.Id == from && k.To.Id == to {
 			delete(d.Vertice[from].Arcs, k)
 			temp = k
@@ -122,6 +130,15 @@ func (d *directedGraph) IsExist(id int) bool {
 	return false
 }
 
+//GetVertex returns vertex for given id
+func (d *directedGraph) GetVertex(id int) (*VertexD, error) {
+	if !d.IsExist(id) {
+		return nil, fmt.Errorf("there is no such vertex[id] : %d", id)
+	}
+	return d.Vertice[id], nil
+
+}
+
 //GetArcs returns arcs that origins from given id vertex
 func (d *directedGraph) GetArcs(id int) (map[*Arc]bool, error) {
 	if !d.IsExist(id) {
@@ -133,12 +150,13 @@ func (d *directedGraph) GetArcs(id int) (map[*Arc]bool, error) {
 
 /*GetAdjacents returns all adjacents of the given vertex as struct. This struct has 2 fields;
 
-importer: arrow of the arc(edge) points outward,
+Importers: arrow of the arc(edge) points outward.To clarify vertex[id] -> vertex[importer].
 
-exporter: arrow of the arc(edge) points inward .*/
+Exporters: arrow of the arc(edge) points inward. To clarify vertex[id] <- vertex[exporter] .*/
 func (d *directedGraph) GetAdjacents(id int) (*struct {
 	Importers []*VertexD //Adjacent list which has a relation FROM given id vertex. To clarify vertex[id] -> vertex[importer]
 	Exporters []*VertexD //Adjacent list which has relation TO given id vertex. To clarify vertex[id] <- vertex[exporter]
+	Total     int
 }, error) {
 	if !d.IsExist(id) {
 		return nil, fmt.Errorf("there is no such vertex with id:%d", id)
@@ -146,13 +164,15 @@ func (d *directedGraph) GetAdjacents(id int) (*struct {
 	temp := &struct {
 		Importers []*VertexD //Adjacent list which has a relation FROM given id vertex
 		Exporters []*VertexD //Adjacent list which has relation TO given id vertex
+		Total     int
 	}{}
 	for k, from := range d.Vertice[id].Arcs {
-		fmt.Println(&k.From, from)
 		if from {
 			temp.Importers = append(temp.Importers, k.To)
+			temp.Total++
 		} else {
 			temp.Exporters = append(temp.Exporters, k.From)
+			temp.Total++
 		}
 	}
 	return temp, nil
@@ -170,8 +190,8 @@ func (d *directedGraph) Len() struct {
 
 }
 
-//NotAdjacent error occurs when there is no adjacent found
-var NotAdjacent = fmt.Errorf("not adjacent")
+//NotAdjacentError error occurs when there is no adjacent found
+var NotAdjacentError = fmt.Errorf("not adjacent")
 
 /*IsAdjacent returns boolean and error, if any.
 
@@ -179,20 +199,27 @@ In case of id1->id2 returns true and nil,
 
 inc ase of id1<-id2 returns false and nil,
 
-if there is no adjacency between them returns false and graphs.NotAdjacent error*/
+if there is no adjacency between them returns false and graphs.NotAdjacentError error*/
 func (d *directedGraph) IsAdjacent(id1, id2 int) (bool, error) {
 	if !d.IsExist(id1) || !d.IsExist(id2) {
 		return false, fmt.Errorf("at least one of the vertice(%d or/and %d ) is not exist", id1, id2)
 	}
-	for k, _ := range d.Vertice[id1].Arcs {
-		if k.From.Id == id1 && k.To.Id == id2 {
-			return true, nil
-		}
-		if k.From.Id == id2 && k.To.Id == id1 {
-			return false, nil
+	for arc, from := range d.Vertice[id1].Arcs {
+
+		if from {
+			if arc.From.Id == id1 && arc.To.Id == id2 {
+				return true, nil
+			}
 		}
 	}
-	return false, NotAdjacent
+	for arc, from := range d.Vertice[id1].Arcs {
+		if !from {
+			if arc.From.Id == id2 && arc.To.Id == id1 {
+				return false, nil
+			}
+		}
+	}
+	return false, NotAdjacentError
 }
 
 //UndirectedGraph is a struct for undirected graph
@@ -211,13 +238,13 @@ type edge struct {
 https://en.wikipedia.org/wiki/Graph_(abstract_data_type)
 The basic operations provided by a graph data structure G usually include:[1]
 
-adjacent(G, x, y): tests whether there is an edge from the vertex x to the vertex y;
-neighbors(G, x): lists all vertices y such that there is an edge from the vertex x to the vertex y;
+//adjacent(G, x, y): tests whether there is an edge from the vertex x to the vertex y;
+//neighbors(G, x): lists all vertices y such that there is an edge from the vertex x to the vertex y;
 //add_vertex(G, x): adds the vertex x, if it is not there;
-remove_vertex(G, x): removes the vertex x, if it is there;
-add_edge(G, x, y): adds the edge from the vertex x to the vertex y, if it is not there;
-remove_edge(G, x, y): removes the edge from the vertex x to the vertex y, if it is there;
-get_vertex_value(G, x): returns the value associated with the vertex x;
+//remove_vertex(G, x): removes the vertex x, if it is there;
+//add_edge(G, x, y): adds the edge from the vertex x to the vertex y, if it is not there;
+//remove_edge(G, x, y): removes the edge from the vertex x to the vertex y, if it is there;
+//get_vertex_value(G, x): returns the value associated with the vertex x;
 set_vertex_value(G, x, v): sets the value associated with the vertex x to v.
 Structures that associate values to the edges usually also provide:[1]
 
